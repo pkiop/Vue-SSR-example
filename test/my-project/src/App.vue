742ce44d2d22963f9747cf7f093d817f5cf92e89
pkiop <pkiop@naver.com>
2021-02-03 12:29:22 +0900

    Feat: Fix vue component

--- test/my-project/src/App.vue
+++ test/my-project/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Counter />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    Counter
   }
 }
 </script>
---
+++ test/my-project/src/components/counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="counter">
+    Counter: {{ counter }}
+    Data: {{ data }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// const Counter = {
+//   data() {
+//     return {
+//       counter: 0
+//     }
+//   }
+// }
+
+const data = 'abc';
+interface twoString {
+  a: string,
+  b: string,
+}
+
+const data2: twoString = {
+  a: 'aa',
+  b: 'bb',
+}
+export default Vue.extend({
+  data() {
+    return {
+      counter: 0,
+      data,
+      data2
+    }
+  }
+})
+</script>
