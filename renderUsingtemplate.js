c749169c4919f34c8475fe4f5399dee0dc0795f8
pkiop <pkiop@naver.com>
2021-02-03 16:18:02 +0900

    Feat: Delete all file

--- renderUsingtemplate.js
+++
@@ -1,7 +0,0 @@
-const renderer = require('vue-server-renderer').createRenderer({
-  template: require('fs').readFileSync('./index.template.html', 'utf-8')
-})
-
-renderer.renderToString(app, (err, html) => {
-  console.log(html) // will be the full page with app content injected.
-})
