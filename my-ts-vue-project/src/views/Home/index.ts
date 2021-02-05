6abde9c551389dc4b2975f2788ca29bce56ff0e2
pkiop <pkiop@naver.com>
2021-02-05 18:35:07 +0900

    Fix: Fix lint error

--- my-ts-vue-project/src/views/Home/index.ts
+++ my-ts-vue-project/src/views/Home/index.ts
@@ -1,11 +1,3 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
-</template>
-
-<script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
@@ -15,4 +7,3 @@ export default defineComponent({
     HelloWorld,
   },
 });
-</script>
---
+++ my-ts-vue-project/src/views/Home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script src="./index.ts"></script>
