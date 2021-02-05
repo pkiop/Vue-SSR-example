ab9e456d629f5e365c4a7df43e29e0e79d1d7925
pkiop <pkiop@naver.com>
2021-02-05 19:24:09 +0900

    Fix: Fix vetur error

--- my-ts-vue-project/src/views/Home/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
